e51f88afc715e2b14c1d762279dfb2b130ab40e5
Arthur <arthurmb2001@hotmail.com>
2024-05-15 16:50:18 -0300

    finishing the project and adding styles

---
+++ README.md
@@ -0,0 +1,6 @@
+# Temperature Converter
+
+A simple temperature converter, with a TypeScript server and only one HTML file as the front end.
+Client requests are made using the fetch() function in a script within the index file itself, and on the server side, Express was used. The styling was applied within the index file because, for some reason, the link wasn't working (lol).
+
+The main goal was to set up a new TypeScript project from scratch, without the help of any IDE, and trying to apply best practices as much as possible.
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,30 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, Router } from "express";
 import path from "path";
+import cors from 'cors';
 
 const app: Express = express();
+const router = Router();
+
+app.use(cors());
+
+router.route('/convert')
+    .post((req: Request, res: Response) => {
+        const {celsiusTemperature, fahrenheitTemperature} = req.body;
+
+        let newFahrenheitTemperature: number = celsiusTemperature ? (celsiusTemperature * 1.8) + 32 : 0;
+        let newCelsiusTemperature: number = fahrenheitTemperature ? (fahrenheitTemperature - 32) / 1.8: 0;
+
+        res.status(200).send({
+            celsiusTemperature: newCelsiusTemperature,
+            fahrenheitTemperature: newFahrenheitTemperature,
+        });
+    });
 
 app.use(express.json());
 
+app.use(router);
 app.get('*', (req: Request, res: Response) => {
-    res.status(200).sendFile(path.resolve('../../client/index.html'));
+    res.status(200).sendFile(path.resolve('../client/index.html'));
 })
 
 
